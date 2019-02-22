4cc474789ede6228d63c6ae9083e1ac2df10739c
rudiwijaya <rudi.wijaya@mitrais.com>
2019-02-22 08:32:25 +0700

    pods

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -13,7 +13,7 @@ class WeatherViewController: UIViewController {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "6539a2ac14afbfd211405e7baef6622e"
     /***Get your own App ID at https://openweathermap.org/appid ****/
     
 
